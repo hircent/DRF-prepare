44c8c4366215308d2c89002a59220b90bd2a2f07
hircent <hircent@gmail.com>
2024-08-19 15:14:18 +0800

    first commit

---
+++ DRF/api/urls.py
@@ -0,0 +1,17 @@
+
+from django.urls import path
+from . import views
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView
+)
+
+urlpatterns = [
+    path('blogpost/', views.BlogPostListCreate.as_view(),name="blogpost-view-create"),
+    path('blogpostlist/', views.BlogPostList.as_view(),name="blogpost-list"),
+    path('blogpost/<int:pk>/', views.BlogPostRetrieveUpdateDestroy.as_view(),name="blogpost-update-destroy"),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+]
