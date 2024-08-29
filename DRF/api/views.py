9e5a78af638654ed495c32c2a0397943b522e130
hircent <hircent@gmail.com>
2024-08-29 15:22:26 +0800

    minor change for file import error

--- DRF/api/views.py
+++ DRF/api/views.py
@@ -1,6 +1,6 @@
 from rest_framework_simplejwt.views import TokenObtainPairView
 
-from DRF.api.serializers import CustomTokenObtainPairSerializer
+from .serializers import CustomTokenObtainPairSerializer
 
 class CustomTokenObtainPairView(TokenObtainPairView):
     serializer_class = CustomTokenObtainPairSerializer
