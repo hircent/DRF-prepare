b0bdcbe7a91921f9dbdfb8ab9212b0d677e84b78
hircent <hircent@gmail.com>
2025-02-11 11:56:59 +0800

    Learn select_related query

--- DRF/api/management/commands/testing.py
+++ DRF/api/management/commands/testing.py
@@ -2,7 +2,7 @@ from django.core.management.base import BaseCommand
 from django.db.models import Q,F ,Value
 from branches.models import Branch
 from calendars.models import CalendarThemeLesson,Calendar
-from classes.models import Class,StudentEnrolment,ClassLesson,StudentAttendance,EnrolmentExtension
+from classes.models import Class,StudentEnrolment,ClassLesson,StudentAttendance,EnrolmentExtension,ReplacementAttendance
 from students.models import Students
 from accounts.models import User
 from category.models import Category,ThemeLesson
@@ -13,6 +13,19 @@ class Command(BaseCommand):
     help = 'testing function'
 
     def handle(self, *args, **kwargs):
-        ext = StudentAttendance.objects.filter(id=236460)
+        ext = ReplacementAttendance.objects.get(attendances_id=236454)
 
         print(ext)
+
+    def learn(self):
+        # Without select_related
+        # This will make N+1 queries (1 for ReplacementAttendance, N for each related StudentAttendance)
+        replacement = ReplacementAttendance.objects.all()
+        for r in replacement:
+            print(r.attendances.student.name)  # Makes a new query for each attendances access
+
+        # With select_related
+        # This makes only 1 query with JOINs
+        replacement = ReplacementAttendance.objects.select_related('attendances', 'attendances__student')
+        for r in replacement:
+            print(r.attendances.student.name)  # No additional queries needed
