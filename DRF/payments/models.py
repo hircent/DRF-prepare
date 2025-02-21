98bf5d45880ad963c30eebb598eb4def279fe0f6
lapendaz-hircent <hircentong.lapendaz@gmail.com>
2025-02-21 13:42:40 +0800

    Certificate app added

--- DRF/payments/models.py
+++ DRF/payments/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Payment(models.Model):
+    pass
+
+class Invoice(models.Model):
+    pass
+
+class InvoiceSequence(models.Model):
+    pass
