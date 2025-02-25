4b215e855385802bf546b399d54ae2707ae12fcc
lapendaz-hircent <hircentong.lapendaz@gmail.com>
2025-02-25 16:00:39 +0800

    add id for display

--- DRF/payments/admin.py
+++ DRF/payments/admin.py
@@ -2,13 +2,13 @@ from django.contrib import admin
 from .models import InvoiceSequence,Invoice,Payment
 # Register your models here.
 class InvoiceSequenceAdmin(admin.ModelAdmin):
-    list_display = ('branch','number')
+    list_display = ('id','branch','number','year')
 
 class InvoiceAdmin(admin.ModelAdmin):
-    list_display = ('branch','invoice_sequence','created_at')
+    list_display = ('id','branch','invoice_sequence','created_at')
 
 class PaymentAdmin(admin.ModelAdmin):
-    list_display = ('enrolment','invoice','amount','paid_amount','status','description')
+    list_display = ('id','enrolment','invoice','amount','paid_amount','status','description')
 
 admin.site.register(InvoiceSequence,InvoiceSequenceAdmin)
 admin.site.register(Invoice,InvoiceAdmin)    
