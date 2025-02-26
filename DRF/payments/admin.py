4b85952b9fe810ea02b8a230a79a05f96d0fa27b
lapendaz-hircent <hircentong.lapendaz@gmail.com>
2025-02-26 11:16:49 +0800

    Add search fields and list filter

--- DRF/payments/admin.py
+++ DRF/payments/admin.py
@@ -4,15 +4,23 @@ from .models import InvoiceSequence,Invoice,Payment
 class InvoiceSequenceAdmin(admin.ModelAdmin):
     list_display = ('id','branch','number','year')
 
+    search_fields = ('branch__name',)
+
 class InvoiceAdmin(admin.ModelAdmin):
     list_display = ('id','branch','invoice_sequence','created_at')
 
     raw_id_fields = ('invoice_sequence','branch')
 
+    search_fields = ('branch__name',)
+
 class PaymentAdmin(admin.ModelAdmin):
-    list_display = ('id','enrolment','invoice','amount','paid_amount','status','description')
+    list_display = ('id','enrolment','enrolment_id','invoice','amount','paid_amount','status','description')
 
     raw_id_fields = ('enrolment','invoice')
+
+    search_fields = ('enrolment__student__fullname',)
+
+    list_filter = ('status',)
 
 admin.site.register(InvoiceSequence,InvoiceSequenceAdmin)
 admin.site.register(Invoice,InvoiceAdmin)    
