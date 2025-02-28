b64511e7f49e6670ef51c25b3c43985bcaf500d5
hircent <hircent@gmail.com>
2025-02-28 16:52:47 +0800

    remove print

--- DRF/api/management/commands/bulk_create_payments.py
+++ DRF/api/management/commands/bulk_create_payments.py
@@ -50,7 +50,7 @@ class Command(CustomBaseCommand):
                             if isinstance(e, User.DoesNotExist):
                                 self.logger.error(f"User with id {row['parent_id']} does not exist in the database.")
                                 raise ImportError(f"Error while importing payments: {str(e)} {row}")
-                        print(row['status'])
+
                         payment = Payment(
                             enrolment_id = row['payable_id'],
                             invoice_id = row['payment_invoice_id'],
