2e25ca78cc566e7db9e5a4f4f2ec9a30d9453add
hircent <hircent@gmail.com>
2025-03-03 11:03:57 +0800

    get_country added

--- DRF/feeStructure/admin.py
+++ DRF/feeStructure/admin.py
@@ -3,14 +3,18 @@ from .models import Tier, Grade, State
 # Register your models here.
 
 class GradeAdmin(admin.ModelAdmin):
-    list_display = ('id', 'grade_level', 'get_tier', 'category', 'price')
-    list_filter = ('category', 'tier')
+    list_display = ('id', 'grade_level', 'get_tier', 'get_country','category', 'price')
+    list_filter = ('category', 'tier','tier__country',)
     search_fields = ('grade_level',)
     ordering = ('grade_level',)
     
     def get_tier(self, obj):
         return obj.tier.name
     get_tier.short_description = 'Tier'
+
+    def get_country(self, obj):
+        return obj.tier.country.name
+    get_country.short_description = 'Country'
 
 class TierAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'year', 'created_at', 'updated_at')
