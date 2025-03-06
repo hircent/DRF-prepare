b83b532afb1157773fb924e115bba35e240de2d2
hircent <hircent@gmail.com>
2025-03-06 10:51:08 +0800

    CRUD view for promo code added

--- DRF/payments/views.py
+++ DRF/payments/views.py
@@ -1,13 +1,17 @@
-from api.global_customViews import BaseCustomListNoPaginationAPIView,BaseCustomListAPIView
+from api.global_customViews import (
+    BaseCustomListNoPaginationAPIView, BaseCustomListAPIView, BasePromoCodeView
+)
 from accounts.permission import IsSuperAdmin,IsPrincipalOrHigher,IsManagerOrHigher,IsTeacherOrHigher
 from datetime import datetime
 from django.db.models import Q
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.generics import UpdateAPIView,DestroyAPIView,CreateAPIView
+from rest_framework.generics import UpdateAPIView,DestroyAPIView,CreateAPIView,RetrieveAPIView
 from rest_framework.exceptions import PermissionDenied
+from rest_framework.response import Response
+from rest_framework import status
 
 from .serializers import (
-    PaymentListSerializer, InvoiceListSerializer, PromoCodeListSerializer
+    PaymentListSerializer, InvoiceListSerializer, PromoCodeSerializer, PromoCodeCreateUpdateSerializer
 )
 from .models import (
     Invoice,Payment,PromoCode
@@ -44,7 +48,7 @@ class InvoiceListView(BaseCustomListAPIView):
 
 class PromoCodeListView(BaseCustomListNoPaginationAPIView):
     permission_classes = [IsSuperAdmin]
-    serializer_class = PromoCodeListSerializer
+    serializer_class = PromoCodeSerializer
 
     def get_queryset(self):
         branch_id = self.get_branch_id()
@@ -63,3 +67,62 @@ class PromoCodeListView(BaseCustomListNoPaginationAPIView):
                 min_purchase_amount__lte=float(purchase_amount)
             )
         )
+    
+class PromoCodeDetailsView(BasePromoCodeView,RetrieveAPIView):
+    permission_classes = [IsSuperAdmin]
+    serializer_class = PromoCodeSerializer
+    
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance)
+        return Response({"success": True, "data": serializer.data}, status=status.HTTP_200_OK)
+    
+class PromoCodeCreateView(BasePromoCodeView,CreateAPIView):
+    permission_classes = [IsSuperAdmin]
+    serializer_class = PromoCodeCreateUpdateSerializer
+
+    def create(self, request, *args, **kwargs):
+        branch_id = self.get_branch_id()
+        
+        data = request.data.copy()
+        data['branch'] = int(branch_id)
+
+        serializer = self.get_serializer(data=data)
+        serializer.is_valid(raise_exception=True)
+
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response({"success": True, "data": serializer.data}, status=status.HTTP_201_CREATED, headers=headers)
+
+class PromoCodeUpdateView(BasePromoCodeView,UpdateAPIView):
+    permission_classes = [IsSuperAdmin]
+    serializer_class = PromoCodeCreateUpdateSerializer
+
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        
+        data = request.data.copy()
+        data['branch'] = int(self.get_branch_id())
+        serializer = self.get_serializer(instance, data=data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+    
+        self.perform_update(serializer)
+        
+        updated_instance = self.get_object()
+        updated_serializer = self.get_serializer(updated_instance)
+        
+        return Response({
+            "success": True,
+            "data": updated_serializer.data
+        })
+
+class PromoCodeDeleteView(BasePromoCodeView,DestroyAPIView):
+    permission_classes = [IsSuperAdmin]
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        id = instance.id
+        self.perform_destroy(instance)    
+        return Response({"success": True, "message": f"Promo Code {id} deleted successfully"})
+    
