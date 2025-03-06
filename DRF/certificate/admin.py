1a87261bcc658d02384aade897dbe9c50c1e1e23
hircent <hircent@gmail.com>
2025-03-06 19:25:48 +0800

    register certificate on admin site

--- DRF/certificate/admin.py
+++ DRF/certificate/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import StudentCertificate
 # Register your models here.
+class StudentCertificateAdmin(admin.ModelAdmin):
+    list_display = ('student','grade','branch','start_date','end_date','status','is_printed')
+    list_filter = ('student','grade','branch')
+
+    raw_id_fields = ('student','grade','branch')
+
+admin.site.register(StudentCertificate,StudentCertificateAdmin)
