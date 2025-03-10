dad599d77eda513afa6a0297562fed59eee2b835
hircent <hircent@gmail.com>
2025-03-10 10:26:48 +0800

    change to void_payments instead of delete_payment, Type indicated

--- DRF/payments/service.py
+++ DRF/payments/service.py
@@ -3,13 +3,21 @@ from django.db.models import Max
 from django.db import transaction
 from django.core.exceptions import ValidationError
 
+from accounts.models import User
+from branches.models import Branch
 from classes.models import StudentEnrolment
+from typing import List
 
 class PaymentService:
 
     @staticmethod
     @transaction.atomic
-    def create_payment(enrolment, amount, parent,branch):
+    def create_payment(enrolment:StudentEnrolment, 
+                       amount:float, 
+                       parent:User,
+                       branch:Branch
+        ):
+
         try:
             new_invoice = PaymentService._create_invoice(branch)
             new_payment = Payment.objects.create(
@@ -24,13 +32,13 @@ class PaymentService:
             raise ValidationError(f"Error creating payment: {str(e)}")
 
     @staticmethod
-    def _create_invoice(branch):
+    def _create_invoice(branch:Branch) -> Invoice:
         invoice_sequence = PaymentService._create_invoice_sequence(branch)
         new_invoice = Invoice.objects.create(invoice_sequence=invoice_sequence,branch=branch)
         return new_invoice
 
     @staticmethod
-    def _create_invoice_sequence(branch):
+    def _create_invoice_sequence(branch:Branch) -> InvoiceSequence:
         max_number = InvoiceSequence.objects.filter(branch=branch).aggregate(Max('number'))['number__max']
 
         if max_number is None:
@@ -40,11 +48,11 @@ class PaymentService:
         return new_invoice_sequence
     
     @staticmethod
-    def delete_payment(enrolment_id):
-        payments = Payment.objects.filter(enrolment_id=enrolment_id)
+    def void_payments(enrolment_ids:List[int],description:str) -> None:
+        payments = Payment.objects.filter(enrolment_id__in=enrolment_ids)
         
         for payment in payments:
             payment.status = 'VOIDED'
-            payment.description = 'Enrolment deleted'
+            payment.description = description
         
         Payment.objects.bulk_update(payments,['status','description'])
