59ddf42693bba1cb0af316d24cd8eec259ad8a47
hircent <hircent@gmail.com>
2025-03-15 15:58:11 +0800

    cronjob mark attendances added

---
+++ DRF/api/management/commands/mark_all_attendances.py
@@ -0,0 +1,102 @@
+from api.baseCommand import CustomBaseCommand
+from api.mixins import BlockedDatesMixin
+from classes.models import ClassLesson,StudentAttendance,Class,ReplacementAttendance
+from branches.models import Branch
+from classes.models import StudentEnrolment
+from django.db import transaction
+from datetime import datetime,date,timedelta
+from typing import List
+
+class Command(CustomBaseCommand,BlockedDatesMixin):
+    help = 'Mark all attendances'
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.logger = self.setup_logger("mark_all_attendances",__name__)
+
+    def add_arguments(self, parser):
+        yesterday = datetime.now() - timedelta(days=1)
+        parser.add_argument('--date', type=date, default=yesterday, help='Date to mark attendances')
+    
+    @transaction.atomic
+    def handle(self, *args, **kwargs):
+        try:
+            date = kwargs['date']
+            all_branches = list(Branch.objects.all().values_list('id',flat=True))
+
+            for branch_id in all_branches:
+                has_lessons = self._is_class_lesson_exists(date,branch_id)
+                
+                if has_lessons:
+                    class_lessons = self._get_class_lessons(date,branch_id)
+                    for cl in class_lessons:
+                        class_instance = cl.class_instance
+                        enrolments = cl.class_instance.enrolments.filter(is_active=True)
+                        replacement_students = cl.class_instance.replacement_attendances.filter(
+                            date=date
+                        ).select_related(
+                            'attendances','attendances__enrollment__student','attendances__enrollment'
+                        )
+
+                        self._create_attendances(date,branch_id,enrolments,class_instance,cl.id)
+                        
+                        if replacement_students:
+                            self._update_replacement(replacement_students,branch_id)
+
+                else:
+                    self.stdout.write(self.style.WARNING(f"No class lessons found for date {date} and branch {branch_id}"))
+                    self.logger.warning(f"No class lessons found for date {date} and branch {branch_id}")
+        except Exception as e:
+            self.stderr.write(self.style.ERROR(f"Error during attendance marking: {str(e)}"))
+            self.logger.error(f"Error during attendance marking: {str(e)}")
+
+    def _is_class_lesson_exists(self,date:date,branch_id:int) -> bool:
+        return ClassLesson.objects.filter(branch_id=branch_id,date=date).exists()
+    
+    def _get_class_lessons(self,date:date,branch_id:int) -> List[ClassLesson]:
+        return ClassLesson.objects.filter(branch_id=branch_id,date=date)
+    
+    def _create_attendances(
+            self,
+            date:date,branch_id:int,
+            enrolments:List[StudentEnrolment],
+            class_instance:Class,class_lesson_id:int
+        ):
+        att_arr = []
+        for en in enrolments:
+            enrolment_id = en.id
+            is_attendance_exists = self._is_attendance_exists(date,branch_id,enrolment_id)
+
+            if not is_attendance_exists:
+                st = StudentAttendance(
+                    enrollment_id=enrolment_id,
+                    branch_id=branch_id,
+                    class_lesson_id=class_lesson_id,
+                    date=date,
+                    day=date.strftime("%A"),
+                    start_time=class_instance.start_time,
+                    end_time=class_instance.end_time,
+                    has_attended=True,
+                    status="ATTENDED"
+                )
+                att_arr.append(st)
+                en.remaining_lessons -= 1
+                en.save()
+
+        if att_arr:
+            StudentAttendance.objects.bulk_create(att_arr)
+            self.stdout.write(self.style.SUCCESS(f"Attendances created for date {date} and branch {branch_id}"))
+            self.logger.info(f"Attendances created for date {date} and branch {branch_id}")
+
+    def _is_attendance_exists(self,date:date,branch_id:int,enrollment_id:int) -> bool:
+        return StudentAttendance.objects.filter(branch_id=branch_id,date=date,enrollment_id=enrollment_id).exists()
+    
+    def _update_replacement(self,replacement_students:List[ReplacementAttendance],branch_id:int) -> None:
+
+        for replacement in replacement_students:
+            replacement.status = "ATTENDED"
+            replacement.attendances.enrollment.remaining_lessons -= 1
+            replacement.attendances.enrollment.save()
+        ReplacementAttendance.objects.bulk_update(replacement_students,["status"])
+        self.stdout.write(self.style.SUCCESS(f"Replacement attendances has been updated for branch id {branch_id}"))
+        
