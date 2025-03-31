45339aa2c0bdb0de9299673c9ee8611e3f760217
hircent <hircent@gmail.com>
2025-03-31 12:13:44 +0800

    get_royalty_fees added, get_attendance_statuses with or without branchId

--- DRF/reports/service.py
+++ DRF/reports/service.py
@@ -1,3 +1,4 @@
+from branches.models import Branch
 from classes.models import StudentAttendance
 from django.db.models import Q,Count,Sum
 from django.db.models.query import QuerySet
@@ -5,6 +6,14 @@ from students.models import Students
 from payments.models import Payment
 
 class PaymentReportService:
+
+    @staticmethod
+    def get_royalty_fees(discounted_amount:int | float,branch:Branch) -> float:
+        if(discounted_amount == 0):
+            return 0
+        percentage = branch.branch_grade.percentage
+        return float(discounted_amount) * (percentage / 100)
+
 
     @staticmethod
     def get_student_statuses(branch_id:int) -> QuerySet[Students]:
@@ -16,15 +25,26 @@ class PaymentReportService:
         )
 
     @staticmethod
-    def get_attendance_statuses(branch_id:int,year:str, month:str) -> QuerySet[StudentAttendance]:
+    def get_attendance_statuses(year: str, month: str, branch_id: int | None = None,country:str | None = None) -> QuerySet[StudentAttendance]:
+
+        if branch_id:
+            return StudentAttendance.objects.filter(
+                branch_id=branch_id,date__year=year,date__month=month
+            ).aggregate(
+                absent=Count('id', filter=Q(status='ABSENT')),
+                freeze=Count('id', filter=Q(status='FREEZED')),
+                sfreezed=Count('id', filter=Q(status='SFREEZED')),
+                replacement=Count('id', filter=Q(status='REPLACEMENT'))
+            )
+        
         return StudentAttendance.objects.filter(
-            branch_id=branch_id,date__year=year,date__month=month
-        ).aggregate(
-            absent=Count('id', filter=Q(status='ABSENT')),
-            freeze=Count('id', filter=Q(status='FREEZED')),
-            sfreezed=Count('id', filter=Q(status='SFREEZED')),
-            replacement=Count('id', filter=Q(status='REPLACEMENT'))
-        )
+                branch__country__name=country,date__year=year,date__month=month
+            ).aggregate(
+                absent=Count('id', filter=Q(status='ABSENT')),
+                freeze=Count('id', filter=Q(status='FREEZED')),
+                sfreezed=Count('id', filter=Q(status='SFREEZED')),
+                replacement=Count('id', filter=Q(status='REPLACEMENT'))
+            )
 
     @staticmethod
     def get_total_amount_of_the_month(branch_id:int,year:str, month:str) -> QuerySet[Payment]:
