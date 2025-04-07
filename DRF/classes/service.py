27ff29aa1af7743097d24d9dbc4d31a503650420
hircent <hircent@gmail.com>
2025-04-07 09:52:37 +0800

    EnrolmentService added

--- DRF/classes/service.py
+++ DRF/classes/service.py
@@ -1,4 +1,5 @@
 from classes.models import StudentEnrolment,Class,VideoAssignment
+from django.db.models import Max
 
 class VideoAssignmentService:
     @staticmethod
@@ -9,3 +10,42 @@ class VideoAssignmentService:
                 VideoAssignment(enrolment=enrolment_instance, video_number=2)
             ]
         VideoAssignment.objects.bulk_create(va_arr)
+
+class EnrolmentService:
+    @staticmethod
+    def deactivate_enrolments(student_id:int)->None:
+        enrolments = StudentEnrolment.objects.filter(student_id=student_id)
+
+        if not enrolments.exists():
+            raise Exception("No enrolment found for student id")
+        
+        enrolments.update(is_active=False)
+
+    @staticmethod
+    def graduate_enrolment(student_id:int)->None:
+        enrolments = StudentEnrolment.objects.filter(student_id=student_id,grade__grade_level=6)
+
+        if not enrolments.exists():
+            raise Exception("No enrolment grade 6 found for this student")
+        
+        grade6 = enrolments.first()
+        grade6.status = 'COMPLETED'
+        grade6.is_active = False
+        grade6.save()
+
+    @staticmethod
+    def activate_latest_enrolment(student_id:int)->None:
+        enrolments = StudentEnrolment.objects.filter(student_id=student_id)
+        
+        if not enrolments.exists():
+            raise Exception("No enrolment found for student id")
+        
+        max_level = enrolments.aggregate(
+            max_level=Max('grade__grade_level')
+        )['max_level']
+
+        latest_enrolment = enrolments.filter(grade__grade_level=max_level).first()
+
+        latest_enrolment.is_active = True
+        latest_enrolment.status = 'IN_PROGRESS'
+        latest_enrolment.save()
