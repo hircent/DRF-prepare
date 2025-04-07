2490349208644af8e55d299d80d84b66e582adc7
hircent <hircent@gmail.com>
2025-04-07 20:23:59 +0800

    CertificateService added

---
+++ DRF/certificate/service.py
@@ -0,0 +1,20 @@
+from certificate.models import StudentCertificate
+from datetime import datetime,date
+
+class CertificateService:
+
+    @staticmethod
+    def generate_certificate(student_id:int, branch_id:int,start_date:str,grade:int) -> None:
+        StudentCertificate.objects.create(
+            student_id=student_id,
+            branch_id=branch_id,
+            grade=grade,
+            start_date=start_date,
+            end_date=datetime.today().strftime("%Y-%m-%d"),
+            status='COMPLETED',
+        )
+
+
+    @staticmethod
+    def destory_certificate(student_id:int, grade_level:int) -> None:
+        StudentCertificate.objects.filter(student_id=student_id,grade=grade_level).delete()
