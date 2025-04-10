2bcbcdceeee4e907f7305dcc54d228b3c1c74cdd
hircent <hircent@gmail.com>
2025-04-10 15:49:36 +0800

    removed PaymentReport

--- DRF/reports/admin.py
+++ DRF/reports/admin.py
@@ -2,7 +2,3 @@ from django.contrib import admin
 from .models import PaymentReport
 # Register your models here.
 
-class PaymentReportAdmin(admin.ModelAdmin):
-    list_display = ('branch','action','action_datetime')
-
-admin.site.register(PaymentReport,PaymentReportAdmin)
