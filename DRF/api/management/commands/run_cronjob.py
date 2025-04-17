162997894fecc1c8dbd35507e398cd9e637c4b13
hircent <hircent@gmail.com>
2025-04-17 17:43:56 +0800

    try cronjob

--- DRF/api/management/commands/run_cronjob.py
+++ DRF/api/management/commands/run_cronjob.py
@@ -1,8 +1,12 @@
-from django.core.management.base import BaseCommand
+from api.baseCommand import CustomBaseCommand
 from datetime import datetime
 
-class Command(BaseCommand):
+class Command(CustomBaseCommand):
+
     help = 'testing function'
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.logger = self.setup_logger("testing_cronjob",__name__)
 
     def handle(self, *args, **kwargs):
-        print(f"Time now is {datetime.now()}")
+        self.logger.info(f"Cronjob started at {datetime.now()}")
